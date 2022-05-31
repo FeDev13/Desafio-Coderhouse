24b71c129d86dc8ab783d9d1984eafa75b5a8837
Federico Robledo <federicorobledo85@gmail.com>
2022-05-31 13:51:29 -0300

    added while-changed index.html

--- main.js
+++ main.js
@@ -1,6 +1,12 @@
 let cliente = prompt("ingrese cliente");
 let dias = parseInt(prompt("ingrese cantidad de dias desde la notificacion"));
-
+while (cliente === "" || dias === "") {
+  alert("datos incompletos");
+  cliente = prompt("ingrese cliente nuevamente");
+  dias = parseInt(
+    prompt("ingrese cantidad de dias desde la notificacion nuevamente")
+  );
+}
 for (i = 0; i < dias; i++) {
   if (dias <= 10) {
     console.log("cliente al dia");
